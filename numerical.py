4c0bcb6d700cf0b33633def0e3ae281ea87bf280
tejasnj001-jpg <tejasnj001@gmail.com>
2026-08-10 14:10:21 +0530

    Add files via upload

---
+++ numerical.py
@@ -0,0 +1,8 @@
+marks = [78, 89, 78, 56]
+
+new_marks = []
+
+for x in marks:
+    new_marks.append(x + 5)
+
+print(new_marks)
---
+++ random.py
@@ -0,0 +1,28 @@
+#random
+import random
+
+print(random.random())
+
+
+#randint
+import random
+
+print(random.randint(1, 10))
+
+#choice
+import random
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print(random.choice(fruits))
+
+
+#shuffle
+
+import random
+
+numbers = [1, 2, 3, 4, 5]
+
+random.shuffle(numbers)
+
+print(numbers)
